809417fb618b19714a9922d723c62444558d3435
vale-sch <vali.vogt@live.de>
2022-03-15 20:16:39 +0100

    inital commit

--- Gyroscope.js
+++ Gyroscope.js
@@ -1,18 +1,20 @@
 
+init();
+
+function init() {
+    let sensor = new Gyroscope();
+    sensor.start();
 
 
-let sensor = new Gyroscope();
-sensor.start();
 
+    sensor.onreading = () => {
+        let x = document.getElementById("x");
+        let y = document.getElementById("y");
+        let z = document.getElementById("z");
+        x.innerHTML = "Angular velocity around the X-axis " + sensor.x;
+        y.innerHTML = "Angular velocity around the Y-axis " + sensor.y;
+        z.innerHTML = "Angular velocity around the Z-axis " + sensor.z;
+    };
+}
 
-let x = document.getElementById("x");
-let y = document.getElementById("y");
-let z = document.getElementById("z");
-x.innerText = "Angular velocity around the X-axis "
-sensor.onreading = () => {
-
-    /* x.innerHTML = "Angular velocity around the X-axis " + sensor.x;
-     y.innerHTML = "Angular velocity around the Y-axis " + sensor.y;
-     z.innerHTML = "Angular velocity around the Z-axis " + sensor.z;*/
-};
 
