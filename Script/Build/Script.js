052e8df6da6dd68c7450e91fa95c4a646761f2b3
vale-sch <vali.vogt@live.de>
2022-03-17 13:33:09 +0100

    init fudge project

--- Script/Build/Script.js
+++ Script/Build/Script.js
@@ -43,19 +43,15 @@ var Script;
     let viewport;
     let madeMazeGraph;
     document.addEventListener("interactiveViewportStarted", start);
-    window.addEventListener("load", init);
     async function init() {
-        // load resources referenced in the link-tag
         await FudgeCore.Project.loadResourcesFromHTML();
         FudgeCore.Debug.log("Project:", FudgeCore.Project.resources);
-        // pick the graph to show
         madeMazeGraph = (f.Project.resources["Graph|2022-03-16T16:05:06.910Z|36331"]);
         FudgeCore.Debug.log("Graph:", madeMazeGraph);
         if (!madeMazeGraph) {
             alert("Nothing to render. Create a graph with at least a mesh, material and probably some light");
             return;
         }
-        // setup the viewport
         let cmpCamera = new FudgeCore.ComponentCamera();
         cmpCamera.mtxPivot.translateY(80);
         cmpCamera.mtxPivot.translateX(5);
@@ -65,29 +61,20 @@ var Script;
         let viewport = new FudgeCore.Viewport();
         viewport.initialize("InteractiveViewport", madeMazeGraph, cmpCamera, canvas);
         FudgeCore.Debug.log("Viewport:", viewport);
-        // hide the cursor when interacting, also suppressing right-click menu
         canvas.addEventListener("mousedown", canvas.requestPointerLock);
         canvas.addEventListener("mouseup", function () { document.exitPointerLock(); });
-        // make the camera interactive (complex method in FudgeAid)
-        // setup audio
-        // let cmpListener = new f.ComponentAudioListener();
-        // cmpCamera.node.addComponent(cmpListener);
-        //FudgeCore.AudioManager.default.listenWith(cmpListener);
-        // FudgeCore.AudioManager.default.listenTo(graph);
         FudgeCore.Debug.log("Audio:", FudgeCore.AudioManager.default);
-        // draw viewport once for immediate feedback
         viewport.draw();
         canvas.dispatchEvent(new CustomEvent("interactiveViewportStarted", { bubbles: true, detail: viewport }));
     }
     (document.head.querySelector("meta[autoView]").getAttribute("autoView"));
-    let rgdbdyBall;
     function start(_event) {
+        init();
         viewport = _event.detail;
-        rgdbdyBall = madeMazeGraph.getChild(0).getComponent(f.ComponentRigidbody);
         let accelButt = document.getElementById("accelButton");
         accelButt.addEventListener("click", getAccel);
         f.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        f.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        f.Loop.start();
     }
     function update(_event) {
         f.Physics.simulate(); // if physics is included and used
@@ -95,36 +82,27 @@ var Script;
         //f.AudioManager.default.update();
     }
     function getAccel() {
-        console.log("jojo");
         let _iOSDevice = !!navigator.platform.match(/iPhone|iPod|iPad/);
         let xAccelartion = document.getElementById("x");
         let yAccelartion = document.getElementById("y");
         let zAccelartion = document.getElementById("z");
+        let rgdbdyBall = madeMazeGraph.getChild(0).getComponent(f.ComponentRigidbody);
         if (_iOSDevice)
             DeviceMotionEvent.requestPermission().then((response) => {
                 if (response == 'granted') {
-                    document.write(response);
-                    // Add a listener to get smartphone orientation 
-                    // in the alpha-beta-gamma axes (units in degrees)
+                    console.log("access acceleration:: " + response);
                     window.addEventListener('deviceorientation', (event) => {
-                        // Expose each orientation angle in a more readable way
                         xAccelartion.innerHTML = "X: " + (-event.gamma).toString();
                         yAccelartion.innerHTML = "Y: " + (-event.alpha).toString();
                         zAccelartion.innerHTML = "Z: " + (-event.beta).toString();
                         rgdbdyBall.applyForce(new f.Vector3(-event.gamma, event.alpha / 10, -event.beta));
-                        //let rotation_degrees: number = event.alpha;
-                        //let frontToBack_degrees: number = event.beta;
-                        //let leftToRight_degrees: number = event.gamma;
-                        // document.writeln(frontToBack_degrees.toString());
-                        // document.writeln(leftToRight_degrees.toString());
-                        // Update velocity according to how tilted the phone is
-                        // Since phones are narrower than they are long, double the increase to the x velocity
                     });
                 }
                 else {
-                    document.write(response);
+                    console.log("access acceleration: " + response);
                 }
             });
+        init();
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- Script/Source/Main.ts
+++ Script/Source/Main.ts
@@ -5,13 +5,10 @@ namespace Script {
   let viewport: f.Viewport;
   let madeMazeGraph: f.Graph;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
-  window.addEventListener("load", init);
   async function init() {
-
-    // load resources referenced in the link-tag
     await FudgeCore.Project.loadResourcesFromHTML();
     FudgeCore.Debug.log("Project:", FudgeCore.Project.resources);
-    // pick the graph to show
+
     madeMazeGraph = <f.Graph>(
       f.Project.resources["Graph|2022-03-16T16:05:06.910Z|36331"]
     );
@@ -20,7 +17,6 @@ namespace Script {
       alert("Nothing to render. Create a graph with at least a mesh, material and probably some light");
       return;
     }
-    // setup the viewport
     let cmpCamera = new FudgeCore.ComponentCamera();
     cmpCamera.mtxPivot.translateY(80);
     cmpCamera.mtxPivot.translateX(5);
@@ -30,30 +26,24 @@ namespace Script {
     let viewport = new FudgeCore.Viewport();
     viewport.initialize("InteractiveViewport", madeMazeGraph, cmpCamera, canvas);
     FudgeCore.Debug.log("Viewport:", viewport);
-    // hide the cursor when interacting, also suppressing right-click menu
     canvas.addEventListener("mousedown", canvas.requestPointerLock);
     canvas.addEventListener("mouseup", function () { document.exitPointerLock(); });
-    // make the camera interactive (complex method in FudgeAid)
-    // setup audio
-    // let cmpListener = new f.ComponentAudioListener();
-    // cmpCamera.node.addComponent(cmpListener);
-    //FudgeCore.AudioManager.default.listenWith(cmpListener);
-    // FudgeCore.AudioManager.default.listenTo(graph);
+
     FudgeCore.Debug.log("Audio:", FudgeCore.AudioManager.default);
-    // draw viewport once for immediate feedback
     viewport.draw();
     canvas.dispatchEvent(new CustomEvent("interactiveViewportStarted", { bubbles: true, detail: viewport }));
 
   } (document.head.querySelector("meta[autoView]").getAttribute("autoView"));
-  let rgdbdyBall: f.ComponentRigidbody
+
   function start(_event: CustomEvent): void {
+    init();
     viewport = _event.detail;
-    rgdbdyBall = madeMazeGraph.getChild(0).getComponent(f.ComponentRigidbody);
+
     let accelButt: HTMLElement = document.getElementById("accelButton");
     accelButt.addEventListener("click", getAccel);
 
     f.Loop.addEventListener(f.EVENT.LOOP_FRAME, update);
-    f.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    f.Loop.start();
   }
 
   function update(_event: Event): void {
@@ -62,39 +52,25 @@ namespace Script {
     //f.AudioManager.default.update();
   }
   function getAccel() {
-    console.log("jojo");
     let _iOSDevice: boolean = !!navigator.platform.match(/iPhone|iPod|iPad/);
     let xAccelartion: HTMLElement = document.getElementById("x");
     let yAccelartion: HTMLElement = document.getElementById("y");
     let zAccelartion: HTMLElement = document.getElementById("z");
+    let rgdbdyBall: f.ComponentRigidbody = madeMazeGraph.getChild(0).getComponent(f.ComponentRigidbody);
     if (_iOSDevice)
       (DeviceMotionEvent as any).requestPermission().then((response: string) => {
         if (response == 'granted') {
-          document.write(response);
-          // Add a listener to get smartphone orientation 
-          // in the alpha-beta-gamma axes (units in degrees)
+          console.log("access acceleration:: " + response);
           window.addEventListener('deviceorientation', (event) => {
-            // Expose each orientation angle in a more readable way
-
             xAccelartion.innerHTML = "X: " + (-event.gamma).toString();
             yAccelartion.innerHTML = "Y: " + (-event.alpha).toString();
             zAccelartion.innerHTML = "Z: " + (-event.beta).toString();
-
             rgdbdyBall.applyForce(new f.Vector3(-event.gamma, event.alpha / 10, -event.beta));
-            //let rotation_degrees: number = event.alpha;
-            //let frontToBack_degrees: number = event.beta;
-            //let leftToRight_degrees: number = event.gamma;
-            // document.writeln(frontToBack_degrees.toString());
-            // document.writeln(leftToRight_degrees.toString());
-            // Update velocity according to how tilted the phone is
-            // Since phones are narrower than they are long, double the increase to the x velocity
-
-
-
           });
         } else {
-          document.write(response);
+          console.log("access acceleration: " + response);
         }
       });
+    init();
   }
 }
