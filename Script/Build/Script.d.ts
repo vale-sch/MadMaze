9917d1a0d4f897270f16bc5577f73261d6e2c8d3
vale-sch <vali.vogt@live.de>
2022-03-31 14:25:18 +0200

    get rid off customcs

--- Script/Build/Script.d.ts
+++ Script/Build/Script.d.ts
@@ -1,20 +1,4 @@
 declare namespace MadMaze {
-    import f = FudgeCore;
-    class CustomComponentScript extends f.ComponentScript {
-        static readonly iSubclass: number;
-        private isCross;
-        private verticalNeg;
-        private verticalPos;
-        private rndRotVel;
-        private rndTransVel;
-        private mySelf;
-        constructor();
-        hndEvent: (_event: Event) => void;
-        private update;
-        private randomRangeFromInterval;
-    }
-}
-declare namespace MadMaze {
 }
 declare namespace MadMaze {
     import f = FudgeCore;
